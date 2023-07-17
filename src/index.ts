93913f9d4149f697ad142f2bb6db28c47091ddd0
keating <keating.dev@protonmail.com>
2023-07-17 12:56:07 -0400

    Add example

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,69 @@
-'use strict';
+import { ethers } from "ethers";
 
-import { Logger } from 'sitka';
+const main = async () => {
+  const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545"); // Tested with anvil node fork of optimism
 
-export class Example {
-	/* Private Instance Fields */
+  const wallet = new ethers.Wallet(
+    "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", // Tested with anvil account
+    provider
+  );
 
-	private _logger: Logger;
+  const depositTx = await depositEther(
+    wallet,
+    "0x067799acf2594162d2C1218E9ea53D61c51C0826",
+    "10000000000000000000"
+  );
 
-	/* Constructor */
+  await depositTx.wait();
 
-	constructor() {
-		this._logger = Logger.getLogger({ name: this.constructor.name });
-	}
+  const approveTx = await delegateApprove(
+    wallet,
+    "0xfd7bB5F6844a43c5469c972640Eddfa99597a547"
+  );
+  await approveTx.wait();
 
-	/* Public Instance Methods */
+  const openTx = await openPosition(
+    wallet,
+    "0x957d09ff87f3B019e3b3Ff7E133Fd8cc720735d8",
+    ethers.solidityPacked(
+      ["uint8", "uint160", "uint32", "uint96", "uint96"],
+      [4, 0, Math.pow(2, 31), "100", 0]
+    )
+  );
+  await openTx.wait();
+};
 
-	public exampleMethod(param: string): string {
-		this._logger.debug('Received: ' + param);
-		return param;
-	}
-}
+const depositEther = async (
+  wallet: ethers.Wallet,
+  contract: string,
+  value: string
+) => {
+  const txData = {
+    to: contract,
+    value: value,
+  };
+  const d = await wallet.populateTransaction(txData);
+  return await wallet.sendTransaction(d);
+};
+
+const openPosition = async (
+  wallet: ethers.Wallet,
+  contract: string,
+  data: string
+) => {
+  const txData = {
+    to: contract,
+    data: data,
+  };
+  const d = await wallet.populateTransaction(txData);
+  return await wallet.sendTransaction(d);
+};
+
+const delegateApprove = async (wallet: ethers.Wallet, contract: string) => {
+  const abi = ["function approve(address delegate, uint8 actions) external"];
+
+  // Create a contract
+  const c = new ethers.Contract(contract, abi, wallet);
+  return await c.approve("0x957d09ff87f3B019e3b3Ff7E133Fd8cc720735d8", 1);
+};
+main();
